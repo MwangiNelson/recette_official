58c3c90ddda8ccd40321af8ab0cdb1194866ae8c
MwangiNelson <nelsonmwangi197@gmail.com>
2023-02-08 12:09:18 +0300

    Lo

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from 'react'
+import { auth } from '../firebase'
+const AuthContext = React.createContext()
+
+export const AuthProvider = (children) => {
+    const [loading, setLoading] = useState(false)
+    const [currentUser, setCurrentUser] = useState()
+
+    function signup(email, password) {
+        return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    function logout() {
+        return auth.signOut()
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+
+        return unsubscribe
+    }, [])
+
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
+import LoginPage from '../home/login'
 const Tests = () => {
-    let [val,setVal] = useState(0)
+    let [val, setVal] = useState(0)
 
     return (
         <div>
-           
+            <LoginPage />
         </div>
     )
 }
